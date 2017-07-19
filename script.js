98f7cf3b44bc0678885befd810a789d96671eb73
fatu973 <fatima.q832@gmail.com>
2017-07-19 11:49:50 -0500

    fix center header

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
- $(function() {
+$(function() {
     $('a.scroll-down').click (function() {
     	console.log("click");
       $('html, body').animate(
       	{scrollTop: $('section#about').offset().top }, 'slow');
       return false;
     });
-  });
+});
