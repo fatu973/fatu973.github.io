0c6768125fe84fedcc410648c2b789d5a7d76c0e
fatu973 <fatima.q832@gmail.com>
2017-07-23 18:49:00 -0500

    add scroll up airplane

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ $(function() {
       return false;
     });
 });
+
+$(function() {
+    $('a.scroll-top').click (function() {
+    	console.log("click");
+      $('html, body').animate(
+      	{scrollTop: $('section#header').offset().top }, 'slow');
+      return false;
+    });
+});
